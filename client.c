ef2b5af7a0de8ff758933252f61d564ff64550bb
Conner Shipley <conshipl@indiana.edu>
2024-09-02 15:11:22 -0900

    Create user command prompt for client

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,8 +15,10 @@ int main(int argc, char * argv[])
 	struct hostent *hp;
 	struct sockaddr_in sin;
 	char *host;
-	char buf[MAX_LINE];
-	int s;
+	char command_buf[MAX_LINE];
+	char file_buf[MAX_LINE];
+	int command_socket;
+	int file_socket;
 	int len;
 
 	if (argc==2) 
@@ -45,28 +48,88 @@ int main(int argc, char * argv[])
 	sin.sin_port = htons(SERVER_PORT);
 
 	/* active open */
-	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
+	if ((command_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
 	{
 		perror("simplex-talk: socket");
 		exit(1);
 	}
 
-	if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+	if (connect(command_socket, (struct sockaddr *)&sin, sizeof(sin)) < 0)
 	{
 		perror("simplex-talk: connect");
-		close(s);
+		close(command_socket);
 		exit(1);
 	}
-	
-	fp = fopen("test.txt", "r");
 
-	/* main loop: get and send lines of text */
-	while (fgets(buf, MAX_LINE, fp)) 
+	while (fgets(command_buf, sizeof(command_buf), stdin))
 	{
-		send(s, buf, sizeof(buf), 0);
-		bzero(buf, MAX_LINE);
+		command_buf[strcspn(command_buf, "\n")] = 0;
+		
+		if (strcmp(command_buf, "EXIT") == 0)
+		{
+			printf("Exiting client program.");
+			break;
+		}
+		
+		int counter = 0;
+        	char* token = strtok(command_buf, " ");
+		char* command;
+		char* fileName;
+        
+        	while(token != NULL && counter < 2)
+        	{
+            		if (counter == 0)
+			{
+				command = token;
+			}
+			else
+			{
+				fileName = token;
+			}
+            		
+			token = strtok(NULL, " ");
+            		counter++;
+        	}
+        
+		if (strcmp(command, "get") == 0)
+        	{
+            		printf("GET COMMAND\n");
+        	}
+        	else if (strcmp(command, "put") == 0)
+        	{
+        		fp = fopen(fileName, "r");
+		
+			if ((file_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+        		{
+                		perror("simplex-talk: socket");
+                		exit(1);
+        		}
+
+        		if (connect(file_socket, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+        		{
+                		perror("simplex-talk: connect");
+                		close(file_socket);
+                		exit(1);
+        		}
+
+			while (fgets(file_buf, MAX_LINE, fp))
+        		{
+                		printf("%s", file_buf);
+				send(file_socket, file_buf, sizeof(file_buf), 0);
+                		bzero(file_buf, MAX_LINE);
+        		}
+
+        		close(file_socket);
+        		fclose(fp);
+        	}
+        	else
+        	{
+            	printf("Unknown command\n");
+        	}
+
+        	bzero(command_buf, 256);
+		
 	}
 
-	close(s);
-	fclose(fp);
+	close(command_socket);
 }
